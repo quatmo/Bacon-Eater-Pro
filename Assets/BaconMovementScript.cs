505c118f68ab8782fcbcb9f94bb74428c47b84be
Nick Mudry <nick@mudry.me>
2014-10-14 14:27:43 -0400

    Work Started
    Started work on Bacon Eater Pro!

---
+++ Assets/BaconMovementScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaconMovementScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.transform.position = new Vector3(mousePosition.x, mousePosition.y);
+
+    }
+}
---
+++ Assets/BaconStateScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaconStateScript : MonoBehaviour {
+    public string baconState;
+    public float timeInPan;
+	// Use this for initialization
+	void Start () {
+        baconState = "raw";
+        timeInPan = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (baconState == "chewy")
+        {
+
+        }
+        else if (baconState == "crispy")
+        {
+
+        }
+        else if (baconState == "burnt")
+        {
+
+        }
+	}
+
+    void CheckBaconState()
+    {
+        if (timeInPan >= 30)
+        {
+            baconState = "chewy";
+        }
+        else if (timeInPan >= 60)
+        {
+            baconState = "crispy";
+        }
+        else if (timeInPan >= 90)
+        {
+            baconState = "burnt";
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Frying Pan")
+        {
+            timeInPan += Time.deltaTime;
+        }
+    }
+}
