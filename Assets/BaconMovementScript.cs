3610e00b1cbc03bb7458dfaa3b7d5173cf2457f9
Nick Mudry <nick@mudry.me>
2014-10-14 16:30:12 -0400

    You can eat bacon!
    Feed bacon to Flood!

--- Assets/BaconMovementScript.cs
+++ Assets/BaconMovementScript.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class BaconMovementScript : MonoBehaviour {
-
+    public bool onFace;
+    GameObject customer;
 	// Use this for initialization
 	void Start () {
-	
+        onFace = false;
+        customer = GameObject.FindGameObjectWithTag("Customer");
 	}
 	
 	// Update is called once per frame
@@ -18,5 +20,29 @@ public class BaconMovementScript : MonoBehaviour {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         gameObject.transform.position = new Vector3(mousePosition.x, mousePosition.y);
 
+    }
+
+    void OnMouseUp()
+    {
+       if (onFace == true)
+       {
+           customer.SendMessage("StopChewing");
+           Destroy(gameObject);
+       }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Customer")
+        {
+            onFace = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Customer")
+        {
+            onFace = false;
+        }
     }
 }
---
+++ Assets/CustomerAnimatorScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomerAnimatorScript : MonoBehaviour {
+    public Animator animator;
+    float timer;
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Bacon")
+        {
+            animator.SetBool("isChewing", true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Bacon")
+        {
+            animator.SetBool("isChewing", false);
+        }
+    }
+    void StopChewing()
+    {
+            animator.SetBool("isChewing", false);
+    }
+}
