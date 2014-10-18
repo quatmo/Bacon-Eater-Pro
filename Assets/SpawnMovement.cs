da2b1f41eab12ec0ba1666c82dbaa00e4891a126
Nick Mudry <Nick@Nicholass-MacBook-Air.local>
2014-10-18 12:39:46 -0400

    Flood Moves Floodily

---
+++ Assets/SpawnMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMovement : MonoBehaviour {
+	public float timeBetweenMoves;
+	public float timer;
+	public bool moveLeft;
+	public float moveDistance; 
+	// Use this for initialization
+	void Start () {
+		moveLeft = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveTimer();
+	}
+
+	void MoveTimer()
+	{
+		timer += Time.deltaTime;
+		if (timer >= timeBetweenMoves)
+		{
+			Move ();
+			timer = 0f;
+		}
+	}
+
+	void Move () {
+		if (moveLeft)
+		{
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x - moveDistance, 
+			                                            gameObject.transform.position.y,
+			                                            gameObject.transform.position.z);
+		}
+		else 
+		{
+			gameObject.transform.position = new Vector3(gameObject.transform.position.x + moveDistance, 
+			                                            gameObject.transform.position.y,
+			                                            gameObject.transform.position.z);
+		}
+	}
+
+	void MoveLeft () {
+		moveLeft = true;
+	}
+
+	void MoveRight () {
+		moveLeft = false;
+	}
+}
---
+++ Assets/SpawnerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerScript : MonoBehaviour {
+	public GameObject objectToSpawn;
+	//public bool directionIsLeft;
+	public float timeToSpawn; //Time it takes to spawn a new object. 
+	float spawnTimer;
+	public Vector3 spawnPosition;
+
+	// Use this for initialization
+	void Start () {
+		spawnTimer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SpawnTimer ();
+	}
+
+	void SpawnTimer()
+	{
+		spawnTimer += Time.deltaTime;
+		if (spawnTimer > timeToSpawn)
+		{
+			Spawn();
+			spawnTimer = 0f;
+		}
+	}
+
+	void Spawn () {
+		GameObject spawnedObject = (GameObject)Instantiate(objectToSpawn, spawnPosition, new Quaternion(0f,0f,0f,0f));
+	}
+}
